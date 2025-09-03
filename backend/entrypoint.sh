238859b6ca7c2b82a83361e01dc52b1052bb643f
Lucas <12496191+lucashuy@users.noreply.github.com>
2025-09-02 23:57:34 -0700

    Adjust Prophet River crawling time up 30 mins

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -5,7 +5,7 @@ chmod +x /bin/almostprohibited-indexer
 # Alpine defaults to UTC, 7am UTC is 12am PST
 echo "0 7 * * * /bin/almostprohibited-indexer --excluded-retailers prophet-river" >> /var/spool/cron/crontabs/root
 # Prophet River seems to have their maintainance at 1am-4am MT, which is interesting
-echo "0 6 * * * /bin/almostprohibited-indexer --retailers prophet-river" >> /var/spool/cron/crontabs/root
+echo "30 6 * * * /bin/almostprohibited-indexer --retailers prophet-river" >> /var/spool/cron/crontabs/root
 
 crond -b
 
