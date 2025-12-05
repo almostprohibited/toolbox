84d42bc8fadfd895502b2778b20a7efce977c8ff
Lucas <12496191+lucashuy@users.noreply.github.com>
2025-12-04 22:40:51 -0800

    Adjust crawling time

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -3,9 +3,8 @@
 chmod +x /bin/almostprohibited-indexer
 
 # Alpine defaults to UTC, 7am UTC is 12am PST
-echo "0 7 * * * /bin/almostprohibited-indexer --excluded-retailers prophet-river" >> /var/spool/cron/crontabs/root
-# Prophet River seems to have their maintainance at 1am-4am MT, which is interesting
-echo "30 6 * * * /bin/almostprohibited-indexer --retailers prophet-river" >> /var/spool/cron/crontabs/root
+echo "0 7 * * * /bin/almostprohibited-indexer --excluded-retailers prophet-river --excluded-retailers canadas-gun-store" >> /var/spool/cron/crontabs/root
+echo "30 6 * * * /bin/almostprohibited-indexer -r prophet-river -r canadas-gun-store" >> /var/spool/cron/crontabs/root
 
 crond -b
 
