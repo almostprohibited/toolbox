918052844c90b6625d8a0a4209f8fa84b475a4ad
Lucas <12496191+lucashuy@users.noreply.github.com>
2025-12-13 15:05:13 -0800

    Adjust CanadasGunStore crawl time

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -5,7 +5,7 @@ chmod +x /bin/almostprohibited-indexer
 # Alpine defaults to UTC, 7am UTC is 12am PST
 echo "0 7 * * * /bin/almostprohibited-indexer -e prophet-river -e canadas-gun-store" >> /var/spool/cron/crontabs/root
 echo "30 6 * * * /bin/almostprohibited-indexer -r prophet-river" >> /var/spool/cron/crontabs/root
-echo "0 20 * * * /bin/almostprohibited-indexer -r canadas-gun-store" >> /var/spool/cron/crontabs/root
+echo "0 4 * * * /bin/almostprohibited-indexer -r canadas-gun-store" >> /var/spool/cron/crontabs/root
 
 crond -b
 
